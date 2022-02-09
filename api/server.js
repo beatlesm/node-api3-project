79b52fc74f5004705f4817504b42bc00b89113f6
beatlesm <rj.geng@gmail.com>
2022-02-09 11:25:32 -0800

    Task 2: add code to api/server.js. index.js

--- api/server.js
+++ api/server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors')
+
+const usersRouter = require('./users/users-router') 
 
 const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(cors()) // cors() returns a (req, res, nex) => { // stuff and then next() }
 
-// remember express by default cannot parse JSON in request bodies
-
-// global middlewares and the user's router need to be connected here
+server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use((err, req, res, next) => { // eslint-disable-line   
+res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+    })
+})
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const server = require ('./api/server')
+const {  PORT } = require('./secret')
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
